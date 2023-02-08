e6b00fefeb7fee5a01a6d1d8f28fc32eacfa2fab
Sushant Rajhans <Sushantrajhans2222@gmail.com>
2023-02-08 22:30:51 +0530

    profile page minor fix

--- src/screens/ProfileScreen.js
+++ src/screens/ProfileScreen.js
@@ -1,8 +1,11 @@
 import React from "react";
+import { useSelector } from "react-redux";
+import { selectUser } from "../features/userSlice";
 import Nav from "../Nav";
 import "./ProfileScreen.css";
 
 function ProfileScreen() {
+  const user = useSelector(selectUser)
   return (
     <div className="profileScreen">
       <Nav />
@@ -10,6 +13,9 @@ function ProfileScreen() {
           <h1>Edit Profile</h1>
          <div className="profileScreen_info">
           <img src="https://i.pinimg.com/originals/fb/8e/8a/fb8e8a96fca2f049334f312086a6e2f6.png" alt="" />
+          <div className="profileScreen_details">
+            <h2>{user.email}</h2>
+          </div>
           </div>  
       </div>
     </div>
