1edeba26bdc7fbfa41b49054d73404a8cc0f42cc
Sushant Rajhans <Sushantrajhans2222@gmail.com>
2023-02-10 21:58:14 +0530

    profile screen style

--- src/screens/ProfileScreen.js
+++ src/screens/ProfileScreen.js
@@ -1,22 +1,35 @@
 import React from "react";
 import { useSelector } from "react-redux";
 import { selectUser } from "../features/userSlice";
+import { auth } from "../firebase";
 import Nav from "../Nav";
 import "./ProfileScreen.css";
 
 function ProfileScreen() {
-  const user = useSelector(selectUser)
+  const user = useSelector(selectUser);
   return (
     <div className="profileScreen">
       <Nav />
       <div className="profileScreen_body">
-          <h1>Edit Profile</h1>
-         <div className="profileScreen_info">
-          <img src="https://i.pinimg.com/originals/fb/8e/8a/fb8e8a96fca2f049334f312086a6e2f6.png" alt="" />
+        <h1>Edit Profile</h1>
+        <div className="profileScreen_info">
+          <img
+            src="https://i.pinimg.com/originals/fb/8e/8a/fb8e8a96fca2f049334f312086a6e2f6.png"
+            alt=""
+          />
           <div className="profileScreen_details">
             <h2>{user.email}</h2>
+            <div className="profileScreen_plans">
+              <h3>Plans</h3>
+              <button
+                onClick={() => auth.signOut()}
+                className="profileScreen_signOut"
+              >
+                Sign Out
+              </button>
+            </div>
           </div>
-          </div>  
+        </div>
       </div>
     </div>
   );
